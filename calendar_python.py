ad7229cf6fe3a38391a044da7918e1ad49aab1d8
namjyot <94439793+namjyot@users.noreply.github.com>
2023-09-25 10:26:47 +0530

    improvements

--- calendar_python.py
+++ calendar_python.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 # returns True if the year is leap
 def is_year_leap(year):
     if year >= 1582:
@@ -13,64 +12,64 @@ def is_year_leap(year):
         return False
 
 # returns the day corresponding to the date
-def get_day(d,m,y):
+def get_day(day,month,year):
     ldays = [31,29,31,30,31,30,31,31,30,31,30,31]
     nldays = [31,28,31,30,31,30,31,31,30,31,30,31]
-    if m > 12:
+    if month > 12:
         return None
         
-    elif not is_year_leap(y):
-        if nldays[m-1] < d:
+    elif not is_year_leap(year):
+        if nldays[month-1] < day:
             return None
             
-    elif ldays[m-1] < d:
+    elif ldays[month-1] < day:
             return None
         
-    if m == 1 or m == 2:
-        m = m+12
-        y -= 1
+    if month == 1 or month == 2:
+        month = month+12
+        year -= 1
     
     days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
-    n = d + 2*m + (3*(m+1)//5) + y + (y//4) - (y//100) + (y//400) + 2
+    n = day + 2*month + (3*(month+1)//5) + year + (year//4) - (year//100) + (year//400) + 2
     val = n%7
     return days[val]
 
 
 # returns 2D array of a specific month's date like: [[' ', ' ', ' ', ' ', ' ', 1, 2], [3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16], [17, 18, 19, 20, 21, 22, 23], [24, 25, 26, 27, 28, 29, 30]]
 
-def m_calendar(m,y):
+def m_calendar(month,year):
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     nldays = [31,28,31,30,31,30,31,31,30,31,30,31]
     li = []
     temp = []
     i = 0
-    d = 1
+    day = 1
     mdays = 0
     
-    if is_year_leap(y) and m == 2:
+    if is_year_leap(year) and month == 2:
         mdays = 29
         
     else:
-        mdays = nldays[m-1]
+        mdays = nldays[month-1]
             
-    res = get_day(d,m,y)
+    res = get_day(day,month,year)
     while res != days[i]:
         temp.append(' ')
         i+=1
-    while d<=mdays:
+    while day<=mdays:
         for k in range(7-i):
-            if d>mdays:
+            if day>mdays:
                 temp.append(' ')
             else:
-                temp.append(d)
-                d+=1
+                temp.append(day)
+                day+=1
                 i=0
         li.append(temp)
         temp = []
     return li
 
-# returns the dataframe with days columns and blank index
+# returns the dataframe with day columns and blank index
 def clean_cal(df):
     days = {0:'Sun', 1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat'}
     ind = {0:"", 1:"", 2:"", 3:"", 4:"", 5:""}
@@ -79,9 +78,9 @@ def clean_cal(df):
     return df
 
 
-m = int(input("Enter Month: "))
-y = int(input("Enter Year: "))
-cldr = m_calendar(m,y)
+month = int(input("Enter Month: "))
+year = int(input("Enter Year: "))
+cldr = m_calendar(month,year)
 df = pd.DataFrame(cldr)
 df = clean_cal(df)
-print(df) # printing the calendar
+print(df) # printint the calendar
